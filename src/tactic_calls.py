ea9789515f9852c598c5205251520fdee14ad45c
Qurban <qurban.ali@iceanimations.com>
2015-10-27 18:20:36 +0500

    initial commit

---
+++ src/tactic_calls.py
@@ -0,0 +1,123 @@
+'''
+Created on Oct 26, 2015
+
+@author: qurban.ali
+'''
+import sys
+sys.path.append("R:/Pipe_Repo/Projects/TACTIC")
+import tactic_client_lib as tcl
+import os
+from pprint import pprint
+import iutil.symlinks as symlinks
+
+server = None
+
+def setServer():
+    errors = {}
+    global server
+    try:
+        server = tcl.TacticServerStub(server='dbserver', login='tactic',
+                                      password='tactic123',
+                                      project='test_mansour_ep')
+    except Exception as ex:
+        errors['Could not connect to TACTIC'] = str(ex)
+    return errors
+        
+def getProjects():
+    errors = {}
+    projects = []
+    if server:
+        try:
+            projects = server.eval("@GET(sthpw/project.code)")
+        except Exception as ex:
+            errors['Could not get the list of projects'] = str(ex)
+    else:
+        errors['Could not find the TACTIC server'] = ''
+    return projects, errors
+    
+        
+def setProject(project):
+    errors = {}
+    if server:
+        try:
+            server.set_project(project)
+        except Exception as ex:
+            errors['Could not set the project'] = str(ex)
+    else:
+        errors['Could not find the TACTIC server'] = ''
+    return errors
+        
+def getEpisodes():
+    eps = []
+    errors = {}
+    if server:
+        try:
+            eps = server.eval("@GET(vfx/episode.code)")
+        except Exception as ex:
+            errors['Could not get the list of episodes from TACTIC'] = str(ex)
+    else:
+        errors['Could not find the TACTIC server'] = ""
+    return eps, errors
+    
+def getSequences(ep):
+    seqs = []
+    errors = {}
+    if server:
+        try:
+            seqs = server.eval("@GET(vfx/sequence['episode_code', '%s'].code)"%ep)
+        except Exception as ex:
+            errors['Could not get the list of sequences from TACTIC'] = str(ex)
+    else:
+        errors['Could not find the TACTIC server'] = ""
+    return seqs, errors
+
+def getLatestFile(file1, file2):
+    latest = file1
+    if os.path.getmtime(file2) > os.path.getmtime(file1):
+        latest = file2
+    return latest
+    
+def getAssets(ep, seq, context='shaded/combined'):
+    errors = {}
+    asset_paths = []
+    try:
+        maps = symlinks.getSymlinks(server.get_base_dirs()['win32_client_repo_dir'])
+    except Exception as ex:
+        errors['Could not get the maps from TACTIC']
+    if server:
+        try:
+            asset_codes = server.eval("@GET(vfx/asset_in_sequence['sequence_code', '%s'].asset_code)"%seq)
+        except Exception as ex:
+            errors['Could not get the Sequence Assets from TACTIC'] = str(ex)
+        if not asset_codes: return []
+        try:
+            ep_assets = server.query('vfx/asset_in_episode', filters = [('asset_code', asset_codes), ('episode_code', ep)])
+        except Exception as ex:
+            errors['Could not get the Episode Assets from TACTIC'] = str(ex)
+        for ep_asset in ep_assets:
+            try:
+                snapshot = server.get_snapshot(ep_asset, context=context, version=0, versionless=True, include_paths_dict=True)
+            except Exception as ex:
+                errors['Could not get the Snapshot from TACTIC for %s'%ep_asset['asset_code']] = str(ex)
+            #if not snapshot: snapshot = server.get_snapshot(ep_asset, context='shaded', version=0, versionless=True, include_paths_dict=True)
+            if snapshot:
+                paths = snapshot['__paths_dict__']
+                if paths:
+                    newPaths = None
+                    if paths.has_key('maya'):
+                        newPaths = paths['maya']
+                    elif paths.has_key('main'):
+                        newPaths = paths['main']
+                    else:
+                        errors['Could not find a Maya file for %s'%ep_asset['asset_code']] = 'No Maya or Main key found'
+                    if newPaths:
+                        if len(newPaths) > 1:
+                            asset_paths.append(symlinks.translatePath(getLatestFile(*newPaths), maps))
+                        else:
+                            asset_paths.append(symlinks.translatePath(newPaths[0], maps))
+    return asset_paths, errors
+
+if __name__ == "__main__":
+    pass
+    #set_server()
+    #pprint(get_assets('ep09', 'EP09_SQ001'))
---
+++ src/ui.py
@@ -0,0 +1,174 @@
+'''
+Created on Oct 27, 2015
+
+@author: qurban.ali
+'''
+parentWin = None
+try:
+    from uiContainer import uic
+    import qtify_maya_window as qtfy
+    parentWin = qtfy.getMayaWindow()
+except:
+    from PyQt4 import uic
+from PyQt4.QtGui import QMessageBox, qApp
+import os.path as osp
+import qutil
+import tactic_calls as tc
+#reload(tc)
+import cui
+
+rootPath = qutil.dirname(__file__, 2)
+uiPath = osp.join(rootPath, 'ui')
+
+Form, Base = uic.loadUiType(osp.join(uiPath, 'main.ui'))
+class UI(Form, Base):
+    def __init__(self, parent=parentWin):
+        super(UI, self).__init__(parent)
+        self.setupUi(self)
+        
+        self.items = []
+        self.title = 'Add Assets'
+        
+        self.progressBar.hide()
+        self.setWindowTitle(self.title)
+        self.setServer()
+        self.populateProjects()
+        
+        self.projectBox.currentIndexChanged[str].connect(self.setProject)
+        self.epBox.currentIndexChanged[str].connect(self.populateSequences)
+        self.seqBox.currentIndexChanged[str].connect(self.populateAssets)
+        self.addButton.clicked.connect(self.addAssets)
+        self.rigButton.toggled.connect(self.callPopulateAssets)
+        self.shadedCombinedButton.toggled.connect(self.callPopulateAssets)
+    
+    def callPopulateAssets(self):
+        self.populateAssets(self.seqBox.currentText())
+    
+    def showMessage(self, **kwargs):
+        return cui.showMessage(self, title=self.title, **kwargs)
+        
+    def setServer(self):
+        errors = tc.setServer()
+        if errors:
+            self.showMessage(msg=errors.keys()[0], icon=QMessageBox.Critical,
+                             details=errors.values()[0])
+        
+    def populateProjects(self):
+        self.projectBox.clear()
+        self.projectBox.addItem('--Select Project--')
+        projects, errors = tc.getProjects()
+        if errors:
+            self.showMessage(msg='Error occurred while retrieving the list of projects from TACTIC',
+                             icon=QMessageBox.Critical,
+                             details=qutil.dictionaryToDetails(errors))
+        if projects:
+            self.projectBox.addItems(projects)
+            
+    def setProject(self, project):
+        self.epBox.clear()
+        self.epBox.addItem('--Select Episode--')
+        if project != '--Select Project--':
+            errors = tc.setProject(project)
+            if errors:
+                self.showMessage(msg='Error occurred while setting the project on TACTIC',
+                                 icon=QMessageBox.Critical,
+                                 details=qutil.dictionaryToDetails(errors))
+            self.populateEpisodes()
+    
+    def populateEpisodes(self):
+        episodes, errors = tc.getEpisodes()
+        if errors:
+            self.showMessage(msg='Error occurred while retrieving the Episodes',
+                             icon=QMessageBox.Critical,
+                             details=qutil.dictionaryToDetails(errors))
+        self.epBox.addItems(episodes)
+    
+    def populateSequences(self, ep):
+        self.seqBox.clear()
+        self.seqBox.addItem('--Select Sequence--')
+        if ep != '--Select Episode--':
+            seqs, errors = tc.getSequences(ep)
+            if errors:
+                self.showMessage(msg='Error occurred while retrieving the Sequences',
+                                 icon=QMessageBox.Critical,
+                                 details=qutil.dictionaryToDetails(errors))
+            self.seqBox.addItems(seqs)
+    
+    def populateAssets(self, seq):
+        for item in self.items:
+            item.deleteLater()
+        del self.items[:]
+        ep = self.epBox.currentText()
+        if not ep or not seq: return
+        if ep == '--Select Episode--' or seq == '--Select Sequence--': return
+        assets, errors = tc.getAssets(ep, seq, self.getContext())
+        if errors:
+            self.showMessage(msg='Error occurred while retrieving the Assets',
+                             icon=QMessageBox.Critical,
+                             details=qutil.dictionaryToDetails(errors))
+        for asset in assets:
+            item = Item(self, asset)
+            self.itemsLayout.addWidget(item)
+            self.items.append(item)
+            
+    def getContext(self):
+        context = self.shadedCombinedButton.text()
+        if self.rigButton.isChecked():
+            context = self.rigButton.text()
+        return context
+            
+    
+    def addAssets(self):
+        try:
+            self.progressBar.setMaximum(len(self.items))
+            self.progressBar.show( )
+            for i, item in enumerate(self.items):
+                if item.isSelected():
+                    path, num = item.getNameNum()
+                    for _ in range(num):
+                        qutil.addRef(path)
+                self.progressBar.setValue(i+1)
+                qApp.processEvents()
+        except Exception as ex:
+            self.showMessage(msg=str(ex), icon=QMessageBox.Critical)
+        finally:
+            self.progressBar.hide()
+            self.progressBar.setMaximum(0)
+            self.progressBar.setValue(0)
+        
+
+Form1, Base1 = uic.loadUiType(osp.join(uiPath, 'item.ui'))
+class Item(Form1, Base1):
+    def __init__(self, parent=None, name='', num=1):
+        super(Item, self).__init__(parent)
+        self.setupUi(self)
+        self.num = num
+        self.name = name
+        
+        self.populate()
+        
+        self.numBox.valueChanged[int].connect(self.setNum)
+        
+    def setName(self, name):
+        self.name = name
+        self.assetButton.setText(osp.basename(osp.splitext(self.name)[0]))
+    
+    def setNum(self, num):
+        self.numBox.setValue(num)
+        self.num = num
+        
+    def populate(self):
+        self.setName(self.name)
+        self.numBox.setValue(self.num)
+        
+    def isSelected(self):
+        return self.assetButton.isChecked()
+    
+    def getName(self):
+        return self.name
+    
+    def getNum(self):
+        return self.num
+        
+    def getNameNum(self):
+        return self.name, self.num
